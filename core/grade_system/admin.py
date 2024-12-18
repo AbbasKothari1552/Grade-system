6fdd0a1f7955e75d9bcdf89132e7c4e9d45eeb6f
sahal <skshaikhsahal064@gmail.com>
2024-12-18 20:25:19 +0530

    Added Search Filters

--- core/grade_system/admin.py
+++ core/grade_system/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
+from django.db.models import Field
 from .models import Branch, Subject, ExamData, BranchSubjectSemester, StudentInfo, StudentExam, GradeData, Result
 
-admin.site.register(Branch)
-admin.site.register(Subject)
-admin.site.register(ExamData)
-admin.site.register(BranchSubjectSemester)
-admin.site.register(StudentInfo)
-admin.site.register(StudentExam)
-admin.site.register(GradeData)
-admin.site.register(Result)
+#filters
+class customFilter(admin.ModelAdmin):
+    search_fields=["enrollment","name"]
+    #list_display=["spid","name","enrollment","gender","date_of_birth","faculty_name",]
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        # Dynamically set all field names in list_display
+        self.list_display = [field.name for field in model._meta.get_fields() if isinstance(field,Field)]
+ 
+admin.site.register(Branch,customFilter)
+admin.site.register(Subject,customFilter)
+admin.site.register(ExamData,customFilter)
+admin.site.register(BranchSubjectSemester,customFilter)
+admin.site.register(StudentInfo,customFilter)
+admin.site.register(StudentExam,customFilter)
+admin.site.register(GradeData,customFilter)
+admin.site.register(Result,customFilter)
