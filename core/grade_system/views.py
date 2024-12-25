bad47217e63d2cd97cb173d602184c5eae5d45d5
AbbasKothari1552 <ask50405808@gmail.com>
2024-12-25 17:04:22 +0530

    Data entry and backlog subjects display Done.

--- core/grade_system/views.py
+++ core/grade_system/views.py
@@ -24,8 +24,29 @@ def student_grade_view(request, enrollment):
     # Collect grade data and results for each exam
     exam_data = []
     for student_exam in student_exams:
-        # Get grade data for each subject in this exam
-        grades = GradeData.objects.filter(student_exam=student_exam)
+
+        if student_exam.exam_data.exam_type == "REPETER":
+            main_exam = StudentExam.objects.filter(
+                student_info=student_exam.student_info, 
+                exam_data__semester=student_exam.exam_data.semester, 
+                exam_data__exam_type="REGULAR").first()
+            
+            if main_exam:
+                failed_subjects = GradeData.objects.filter(student_exam=main_exam, grade="FF")
+
+                # Get the backlog subjects for the repeater exam
+                grades = GradeData.objects.filter(
+                    student_exam=student_exam,
+                    subject_bss__in=failed_subjects.values_list('subject_bss', flat=True)  # Match only failed subjects
+                )
+            else:
+                print("Main exam not found.")
+        else:
+            # Get grade data for each subject in this exam
+            grades = GradeData.objects.filter(student_exam=student_exam)
+
+        # # Filter grades to include only subjects where grade is 'FF'
+        # failed_grades = grades.filter(grade='FF')
 
         # Get the result for this exam
         result = Result.objects.filter(student_exam=student_exam).first()
